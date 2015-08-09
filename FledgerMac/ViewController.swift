e02204e278f4a5cec44aaf6fb701e7fe3f7fea8c
robert.conrad <robert.conrad@coinposit.com>
2015-08-09 14:00:43 -0700

    Initial Commit

---
+++ FledgerMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FledgerMac
+//
+//  Created by Robert Conrad on 8/9/15.
+//  Copyright (c) 2015 Robert Conrad. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
